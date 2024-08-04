4b45ab9c3afdcd353504550a47e4c08cbcc2afff
raskavil <raskavil@gmail.com>
2024-08-04 21:52:24 +0200

    Added firebase

--- SalinaCompanion/SalinaCompanionApp.swift
+++ SalinaCompanion/SalinaCompanionApp.swift
@@ -3,6 +3,18 @@ import Networking
 import Models
 import Device
 import MapKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+  }
+}
 
 @main
 struct SalinaCompanionApp: App {
